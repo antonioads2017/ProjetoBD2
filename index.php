4014b47721c63525e9b2b48fc11479d308ddf545
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-24 14:21:48 -0300

    pagina inicial em andamento, adicionado os css's e js's

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>AF Informática</title>
+
+	<!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+<body style="background-color: black">
+	
+	<div style="height: 100%; margin-left: 370px; margin-right: 370px;">
+
+		<nav>
+		    <div class="nav-wrapper" style="background-color: white;">
+		      	<ul id="nav-mobile" class="brand-logo center hide-on-med-and-down">
+		        	<li><a href="sass.html" style="color: black">Quem somos</a></li>
+		        	<li><a href="badges.html" style="color: black">Política do site</a></li>
+		        	<li><a href="collapsible.html" style="color: black">Logar/Cadastrar</a></li>
+		      	</ul>
+		  	</div>
+		</nav>
+
+	</div>
+
+	<!--JavaScript at end of body for optimized loading-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+</body>
+</html>
