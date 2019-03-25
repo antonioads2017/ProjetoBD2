7ab908d42e9ff7987b103e279ad2b2e4c6a9163d
antonio <antoniofilhonerde@gmail.com>
2019-03-25 10:36:19 -0300

    modal de login feita

--- index.php
+++ index.php
@@ -7,7 +7,9 @@
 	<!--Import Google Icon Font-->
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+          <link rel="stylesheet" type="text/css" media="screen,project" href="css/materialize.css" />
+        <link rel="stylesheet" type="text/css" media="screen" href="css/materialize.min.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
       <link rel="stylesheet" type="text/css" href="css/header.css">
 
@@ -16,25 +18,50 @@
 
 </head>
 <body>
-	
-	<div class="nav">
-			<nav style="color: yellow; border-radius: 10px;">
+			<nav> 
 	    		<div class="nav-wrapper">
-	      			<a href="#" class="brand-logo" style="margin-left: 10px; font-size: 42px">AF Informática</a>
+	      			<a href="index.php" class="brand-logo left" >AF Informática</a>
 	      			<ul id="nav-mobile" class="right hide-on-med-and-down">
-	        			<li><a href="sass.html">Quem somos</a></li>
-	        			<li><a href="badges.html">Políticas do site</a></li>
-	        			<li><a href="collapsible.html">Logar/Cadastrar</a></li>
+	        			<li><a href="sass.html">Quem Somos?</a></li>
+	        			<li><a href="badges.html">Políticas do Site</a></li>
+	        			<li><a class="btn-large waves-light red btn modal-trigger" href="#modal1">Login</a></li>
+	        			<li><a href="collapsible.html">Cadastrar-se</a></li>
 	      			</ul>
 	    		</div>
 	  		</nav>
-		<div style="background-color: white; height: 100%; margin-top: 20px; border-radius: 10px;">
+
+	<div id="modal1" class="modal">
+		<div class="modal-content">
+			<center>
+			<div class="row">
+				<form class="col10 s10" action="" method="">
+					<div class="row">
+						<div class="input-field col10 s10">
+							<input class="validate" id="email" type="email" required>
+							<label for="email">E-mail</label>
+						</div>
+						<div class="input-field col10 s10">
+							<input class="validate" id="email" type="password" required>
+							<label for="password">Senha</label>
+						</div>
+						<button class="btn waves-effect waves-light red">Login</button>
+					</div>
+				</form>
+			</div>
+			</center>
 		</div>
-
 	</div>
-
-	<!--JavaScript at end of body for optimized loading-->
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/materialize.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script>
+    	$(document).ready(function(){
+    		$('.modal').modal();
+  			});
+     
+    </script>
+
 
 </body>
 </html>
