7878bc579e9f67ee1f095db2bacc862466264802
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-25 21:25:28 -0300

    feito o header para pessoas logadas

---
+++ headerlogged.php
@@ -0,0 +1,37 @@
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+
+	<div class="header">
+		<nav class="headerborder blue"> 
+			<div class="nav-wrapper">
+				<a href="index.php" class="brand-logo headernav">AF Informática</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
+					<li><a class="waves-light red btn modal-trigger" href="#modal1">Meu perfil</a></li>
+					<li><a class="waves-light red btn modal-trigger" href="#modal2">Meus pedidos</a></li>
+				</ul><br>
+				<div class="headermarket">
+					<nav class="blue headerborder">
+			    		<div class="nav-wrapper">
+			      			<form>
+			        			<div class="nav-wrapper">
+			          				<ul id="nav-mobile" class="left hide-on-med-and-down">
+										<li><a class="waves-light red btn modal-trigger" href="#modal1">Carrinho</a></li>
+									</ul>
+									<div class="headersearch headerborder red">
+										<form>
+					        				<div class="input-field">
+
+					          					<input id="search" type="search" required>
+					          					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+					          					<i class="material-icons">close</i>
+					        				</div>
+					      				</form>
+				      				</div>
+			        			</div>
+			      			</form>
+			   			</div>
+					</nav>
+				</div>				
+			</div>
+		</nav>
+	</div>
