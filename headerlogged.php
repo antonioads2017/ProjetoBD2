8ae45e9e606648f7144073a25f3da0f939805659
antonio <antoniofilhonerde@gmail.com>
2019-03-26 10:35:41 -0300

    controle de sessao corrigido

--- headerlogged.php
+++ headerlogged.php
@@ -8,10 +8,12 @@
 					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
 					<li><a class="waves-light red btn modal-trigger" href="#modal1">Meu perfil</a></li>
 					<li><a class="waves-light red btn modal-trigger" href="#modal2">Meus pedidos</a></li>
-					<li><a class="waves-light red btn modal-trigger" href="<?php session_unset();?>">Sair</a></li>
+					<li class="form-inline my-2 my-lg-0" method="post" action="/services/logout.php">
+						<a class="waves-light red btn modal-trigger" type="submit" name="action">Sair</a>
+					</li>
 				</ul><br>
 				<?php
-					include ('carrinho.php')
+				include ('carrinho.php')
 				?>
 				
 			</div>
