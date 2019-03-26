ad63a3984f3df5bf3c421529ff427cd4b54e73d3
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-25 21:23:55 -0300

    feito o header principal

--- header.php
+++ header.php
@@ -5,11 +5,33 @@
 			<div class="nav-wrapper">
 				<a href="index.php" class="brand-logo headernav">AF Informática</a>
 				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a class="waves-light red btn modal-trigger" href="sass.html">Quem Somos?</a></li>
-					<li><a class="waves-light red btn modal-trigger" href="badges.html">Políticas do Site</a></li>
+					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
 					<li><a class="waves-light red btn modal-trigger" href="#modal1">Login</a></li>
 					<li><a class="waves-light red btn modal-trigger" href="#modal2">Cadastrar-se</a></li>
-				</ul>
+				</ul><br>
+				<div class="headermarket">
+					<nav class="blue headerborder">
+			    		<div class="nav-wrapper">
+			      			<form>
+			        			<div class="nav-wrapper">
+			          				<ul id="nav-mobile" class="left hide-on-med-and-down">
+										<li><a class="waves-light red btn modal-trigger" href="#modal1">Carrinho</a></li>
+									</ul>
+									<div class="headersearch headerborder red">
+										<form>
+					        				<div class="input-field">
+
+					          					<input id="search" type="search" required>
+					          					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+					          					<i class="material-icons">close</i>
+					        				</div>
+					      				</form>
+				      				</div>
+			        			</div>
+			      			</form>
+			   			</div>
+					</nav>
+				</div>				
 			</div>
 		</nav>
 	</div>
