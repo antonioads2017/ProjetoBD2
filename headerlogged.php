da58c87e6a9977d9192f5a3eefeff6f1f05f541a
antonio <antoniofilhonerde@gmail.com>
2019-03-25 22:12:52 -0300

    include de carrinho de acordo com a sessão

--- headerlogged.php
+++ headerlogged.php
@@ -8,30 +8,12 @@
 					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
 					<li><a class="waves-light red btn modal-trigger" href="#modal1">Meu perfil</a></li>
 					<li><a class="waves-light red btn modal-trigger" href="#modal2">Meus pedidos</a></li>
+					<li><a class="waves-light red btn modal-trigger" href="<?php session_unset();?>">Sair</a></li>
 				</ul><br>
-				<div class="headermarket">
-					<nav class="blue headerborder">
-			    		<div class="nav-wrapper">
-			      			<form>
-			        			<div class="nav-wrapper">
-			          				<ul id="nav-mobile" class="left hide-on-med-and-down">
-										<li><a class="waves-light red btn modal-trigger" href="#modal1">Carrinho</a></li>
-									</ul>
-									<div class="headersearch headerborder red">
-										<form>
-					        				<div class="input-field">
-
-					          					<input id="search" type="search" required>
-					          					<label class="label-icon" for="search"><i class="material-icons">search</i></label>
-					          					<i class="material-icons">close</i>
-					        				</div>
-					      				</form>
-				      				</div>
-			        			</div>
-			      			</form>
-			   			</div>
-					</nav>
-				</div>				
+				<?php
+					include ('carrinho.php')
+				?>
+				
 			</div>
 		</nav>
 	</div>
