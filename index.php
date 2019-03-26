8f3afb34c99b06b4ebcd7263f34beefc434b6dac
antonio <antoniofilhonerde@gmail.com>
2019-03-26 11:06:02 -0300

    corrigindo conflitos

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	]
 	<link rel="stylesheet" type="text/css" href="css/index.css">
-
+	<link rel="stylesheet" type="text/css" href="css/header.css">
 </head>
 <body style="background-image: url(img/informatica-background-5.jpg)">
 
@@ -27,6 +27,21 @@
 		}
 
 	?>
+		<link rel="stylesheet" type="text/css" href="css/header.css">
+
+	<div class="header">
+		<nav class="headerborder blue"> 
+			<div class="nav-wrapper">
+				<a href="index.php" class="brand-logo headernav">AF Informática</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
+					<li><a class="waves-light red btn modal-trigger" href="#modal1">Login</a></li>
+					<li><a class="waves-light red btn modal-trigger" href="#modal2">Cadastrar-se</a></li>
+				</ul><br>
+								
+			</div>
+		</nav>
+	</div>
 
 	<div class="indexbody white"></div>
 
