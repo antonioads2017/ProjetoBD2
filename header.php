6eee3a47af47275061fc2fb4a133a58f33bebed3
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-26 21:15:53 -0300

    subtraido o botão do quem somos do header

--- header.php
+++ header.php
@@ -1,13 +1,12 @@
 <div class="header">
-		<nav class="headerborder blue"> 
-			<div class="nav-wrapper">
-				<a href="index.php" class="brand-logo headernav">AF Informática</a>
-				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
-					<li><a class="waves-light red btn modal-trigger" href="#modal1">Login</a></li>
-					<li><a class="waves-light red btn modal-trigger" href="#modal2">Cadastrar-se</a></li>
-				</ul><br>
+	<nav class="headerborder blue"> 
+		<div class="nav-wrapper">
+			<a href="index.php" class="brand-logo headernav">AF Informática</a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a class="waves-light red btn modal-trigger" href="#modal1">Login</a></li>
+				<li><a class="waves-light red btn modal-trigger" href="#modal2">Cadastrar-se</a></li>
+			</ul><br>
 								
-			</div>
-		</nav>
-	</div>
+		</div>
+	</nav>
+</div>
