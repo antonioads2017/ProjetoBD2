bae3b023be0b9012f386d53d6d607224ae69c392
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-26 21:18:29 -0300

    adicionado footer a página principal e deletado a página quem somos

--- index.php
+++ index.php
@@ -27,10 +27,7 @@
 		}
 		include ('header.php');
 
-	?>
-		<link rel="stylesheet" type="text/css" href="css/header.css">
-
-	<div class="indexbody white"></div>
+	?>    
 
 	<div id="modal1" class="modal">
 		<div class="modal-content">
@@ -114,6 +111,37 @@
 			});
 
 		</script>
+
+		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text"><span><b>AF Informática</b></span></h5>
+                <p class="grey-text text-lighten-4"><b>Somos um site pioneiro de Hardware e Periféricos, visado no custo por benefício e promoções que vão te deixar boquiaberta.</b></p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Developers</h5>
+                <ul>
+                  	<li><img src="img/edu.jpg" class="circle"><br>
+                  		<span class="black-text">
+	                		<b>Eduardo Lucas</b> Simpático, gosta de informática e development desde os seus 10 anos.
+	              		</span><br>
+                  	</li>
+                  	<li><br><img src="img/tony.jpg" class="circle"><br>
+                  		<span class="black-text">
+	                		<b>Antonio Miguel</b> - Técnico em Informátiva ha 8 anos e boa experiência na área de hardware, analista e desenvolvedor de sistemas em andamento.
+	              		</span>
+                  	</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 AF Informática
+            </div>
+          </div>
+        </footer>
 
 
 	</body>
