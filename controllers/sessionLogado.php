450581db73c6d3f51c5ffa7ed7d0d25c2459a2b5
antonio <antoniofilhonerde@gmail.com>
2019-03-27 19:40:06 -0300

    atribuindo variavel logado na sessao

--- controllers/sessionLogado.php
+++ controllers/sessionLogado.php
@@ -1,9 +1,10 @@
 <?php 
 	session_start();
 
-	if(isset($_SESSION['logado'])){
-		header('location:#');
+	if(!isset($_SESSION['logado'])){
+		unset($_SESSION['logado']);
+		header('location:../index.php');
 	}
-	
+	$logado = $_SESSION['logado'];
 
 ?>
