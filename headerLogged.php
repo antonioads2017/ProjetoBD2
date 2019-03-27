2e60efa76af0b05636ac8057e49d8814c3dcc0cc
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-26 21:16:36 -0300

    subtraido o botão do quem somos do headerlogged

--- headerLogged.php
+++ headerLogged.php
@@ -1,22 +1,21 @@
 <div class="header">
-		<nav class="headerborder blue"> 
-			<div class="nav-wrapper">
-				<a href="index.php" class="brand-logo headernav">AF Informática</a>
-				<ul id="nav-mobile" class="right hide-on-med-and-down">
-					<li><a class="waves-light red btn modal-trigger" href="quemsomos.php">Quem Somos?</a></li>
-					<li><a class="waves-light red btn modal-trigger" href="#modal1">Meu perfil</a></li>
-					<li><a class="waves-light red btn modal-trigger" href="#modal2">Meus pedidos</a></li>
-					<li >
-						<form method="POST" action="/services/logout.php">
-							<button class="waves-light red btn" type="submit" name="action">Sair</button>
-						</form>
-						
-					</li>
-				</ul><br>
+	<nav class="headerborder blue"> 
+		<div class="nav-wrapper">
+			<a href="index.php" class="brand-logo headernav">AF Informática</a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
+				<li><a class="waves-light red btn modal-trigger" href="#modal1">Meu perfil</a></li>
+				<li><a class="waves-light red btn modal-trigger" href="#modal2">Meus pedidos</a></li>
+				<li >
+					<form method="POST" action="/services/logout.php">
+						<button class="waves-light red btn" type="submit" name="action">Sair</button>
+					</form>
+				</li>
+			</ul><br>
+
 				<?php
 				include ('carrinho.php')
 				?>
 				
-			</div>
-		</nav>
-	</div>
+		</div>
+	</nav>
+</div>
