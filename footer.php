b1bdc45b232e754aab4d97bd41369d2207a386ba
eduulucas <killerstiev4@gmail.com>
2019-03-29 07:57:39 -0300

    remodelado a pagina inicial do site

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" type="text/css" href="css/header.css">
 
-<footer class="page-footer headerborder header indigo" >
+<footer class="page-footer headerborder header indigo indexbody" >
           <div class="container offset-l2">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text"><span><b>AF Informática</b></span></h5>
+                <h5 class="white-text"><span><b>Seja bem vindo ao AF Informática</b></span></h5>
                 <p class="grey-text text-lighten-4"><b>Somos um site pioneiro de Hardware e Periféricos, visado no custo por benefício e promoções que vão te deixar boquiaberta.</b></p>
               </div>
               <div class="col l4 offset-l2 s12">
@@ -12,12 +12,12 @@
                 <ul>
                   	<li><img src="img/edu.jpg" class="circle">
                   		<span class="black-text">
-	                		<b>Eduardo Lucas</b> Simpático, gosta de informática e development desde os seus 10 anos.
+	                		<b>Eduardo Lucas</b> Simpático, gosta de informática e programação desde os seus 10 anos.
 	              		</span>
                   	</li>
                   	<li><img src="img/tony.jpg" class="circle">
                   		<span class="black-text">
-	                		<b>Antonio Miguel</b> - Técnico em Informátiva ha 8 anos e boa experiência na área de hardware, analista e desenvolvedor de sistemas em andamento.
+	                		<b>Antonio Miguel</b> - Técnico em Informática ha 8 anos e boa experiência na área de hardware, analista e desenvolvedor de sistemas em andamento.
 	              		</span>
                   	</li>
                 </ul>
