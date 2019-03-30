4e818e7e7de278ab2c3742914ac3c36865688f09
Eduardo Lucas <killerstiev4@gmail.com>
2019-03-29 21:24:16 -0300

    adicionado nova página de meus pedidos

--- headerLogged.php
+++ headerLogged.php
@@ -4,7 +4,7 @@
 			<a href="index.php" class="brand-logo headernav">AF Informática</a>
 			<ul id="nav-mobile" class="right hide-on-med-and-down">
 				<li><a class="waves-light red btn modal-trigger" href="meuPerfil.php">Meu perfil</a></li>
-				<li><a class="waves-light red btn modal-trigger" href="#modal2">Meus pedidos</a></li>
+				<li><a class="waves-light red btn modal-trigger" href="meusPedidos.php">Meus pedidos</a></li>
 				<li >
 					<form method="POST" action="/services/usuario/logout.php">
 						<button class="waves-light red btn" type="submit" name="action">Sair</button>
---
+++ meusPedidos.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>AF Informática</title>
+
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link rel="stylesheet" type="text/css" media="screen,project" href="css/materialize.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="css/materialize.min.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	]
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/header.css">
+	<?php require ("controllers/sessionLogado.php");
+
+	?>
+
+</head>
+<body>
+	<?php 
+	header("Cache-Control: no-cache, must-revalidate");
+	include ('headerLogged.php');
+	include('controllers/message.php');
+	?>
+	<div class="indexbody">
+		<div class="white headerborder left-align valign-wrapper">
+			<img class="imgpedido" src="img/z390_aorus_elite.png">
+			<h6>teste z390</h6>
+		</div>
+	</div>
+	<?php 
+	include ('footer.php');
+	?>
+
+	
+
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/materialize.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('.modal').modal();
+		});
+
+	</script>
+
+
+
+</body>
+</html>
