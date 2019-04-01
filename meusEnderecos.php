bca1679dc7e5f427d7610f7b7874e709d92fa452
Eduardo Lucas <killerstiev4@gmail.com>
2019-04-01 03:16:20 -0300

    feito a página de meusenderecos

--- meusEnderecos.php
+++ meusEnderecos.php
@@ -11,11 +11,14 @@
 
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	]
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 	<link rel="stylesheet" type="text/css" href="css/header.css">
-	<?php require("services/usuario/listarEnderecos.php");
-	require("controllers/sessionLogado.php");?>
+	<?php 	
+	require("services/usuario/listarEnderecos.php");
+	require("controllers/sessionLogado.php");
+	?>
+
+
 </head>
 <body>
 	<?php 
@@ -27,12 +30,35 @@
 		<div class="row">
 			<div class="col s12"></div>
 			<div class="col s12 m4 l2"></div>
-			<div class="col s12 m4 l12 ">
-				<h4 class="white-text center">Meus Endereços</h4>
-				<div class="card">
-					<div class="card-content grey lighten-4">
-						<div>
-								<?php listar($logado);?>
+			
+			<div id="modalendereco" class="modal">
+				<div class="modal-content">
+					<center>
+						<div class="row">
+							<form action="/services/usuario/cadastrarEnderecos.php" method="POST">
+								<div class="row">
+									<div class="input-field col10 s10">
+										<div class="map">
+											<?php include ("googlemaps.php"); ?>
+										</div>
+									</div><br>
+									<input type="text" id="latitude" name="latitude" value="">
+									<input type="text" id="longitude" name="longitude" value="">
+									<button class="btn waves-effect waves-light red" type="submit" name="action">Cadastrar endereço</button>
+								</div>
+							</form>
+						</div>
+					</center>
+				</div>
+			</div>
+
+			<div class="col s12 m4 l12 headerborder">
+				<h4 class="white-text center headerborder">Meus Endereços</h4>
+				<div class="card headerborder">
+					<div class="col s10 m5 l12 card-content grey lighten-4 headerborder">
+						<div class="col s10 m5 l12 headerborder">
+							<?php listar($logado);?>
+							<a href="#modalendereco" class="waves-light btn modal-trigger" >Adicionar Endereço</a>
 						</div>
 					</div>
 				</div>
@@ -52,9 +78,21 @@
 		</div>
 	</div>
 	<?php 
-			include ('footer.php');
-			include ('controllers/message.php')
-		?>
+	include ('footer.php');
+	include ('controllers/message.php')
+	?>
+
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="js/materialize.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script>
+		$(document).ready(function(){
+			$('.modal').modal();
+		});
+
+	</script>
+	<?php require("services/usuario/cadastrarEnderecos.php"); ?>
 
 </body>
 </html>
