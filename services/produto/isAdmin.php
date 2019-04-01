214cb2261002dd1602447d345d922a052d75d53a
Eduardo Lucas <killerstiev4@gmail.com>
2019-04-01 03:18:42 -0300

    n sei pq isso ta aparecendo pra commitar

--- services/produto/isAdmin.php
+++ services/produto/isAdmin.php
@@ -3,7 +3,7 @@
 	function admin($user){
 		$result = buscarMongo('usuario',['email'=>$user]);
 		if($result['admin']=='true'){
-		  echo '<a class="modal-trigger" href="#modal3" >Cadastro</a><br><a class="modal-trigger" href="#modal4" >Solicitar Exclusão</a>';
+		  echo '<a class="modal-trigger waves-effect waves-light btn" href="#modal3" >Cadastrar produto </a><br><br><a class="modal-trigger waves-effect waves-light btn" href="#modal4" >Solicitar Exclusão</a>';
 		}
 	}	
 	;
