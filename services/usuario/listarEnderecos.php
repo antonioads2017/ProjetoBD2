2aaa4a2f8464da83c662799df53072e6b7003ac9
Eduardo Lucas <killerstiev4@gmail.com>
2019-04-02 01:44:26 -0300

    modificado a listagem para aparecer o id para uma futura exclusão

--- services/usuario/listarEnderecos.php
+++ services/usuario/listarEnderecos.php
@@ -3,14 +3,14 @@ require("databases/postgresConnection.php");
 
 function listar ($logado){
 	$con = getConnection();
-
 	$query = "SELECT * FROM endereco WHERE logado = '$logado'";
 	$resultados = pg_query($con,$query);
 	closeConnection($con);
 	if(!empty(pg_num_rows($resultados))){
 		while($resultado = pg_fetch_assoc($resultados)) {
-			echo '<tr><td class="listaendereco">'.$resultado['street'].'</td><td class="listaendereco">'.$resultado['country'].'</td><td class="listaendereco">'.$resultado['province'].'</td><td class="listaendereco">'.$resultado['postal_code'].'</td><td class="listaendereco">'.$resultado['formatted_address'].'</td></tr>';
+			echo '<tr><td class="listaendereco">'.$resultado['id'].'</td><td class="listaendereco">'.$resultado['street'].'</td><td class="listaendereco">'.$resultado['country'].'</td><td class="listaendereco">'.$resultado['province'].'</td><td class="listaendereco">'.$resultado['postal_code'].'</td><td class="listaendereco">'.$resultado['formatted_address'].'</td></tr>';
 		}
+		
 
 	}else{
 		echo '<tr><td>Não há endereços cadastrados</td></tr><br>';
