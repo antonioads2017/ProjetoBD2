ec571a3154226867f8f2952e6ea3638f1287dea9
Eduardo Lucas <killerstiev4@gmail.com>
2019-04-02 01:42:12 -0300

    alterado para comportar o endereço na modal de cadastrar endereço

--- meusEnderecos.php
+++ meusEnderecos.php
@@ -42,8 +42,9 @@
 											<?php include ("googlemaps.php"); ?>
 										</div>
 									</div><br>
-									<input type="text" id="latitude" name="latitude" value="">
-									<input type="text" id="longitude" name="longitude" value="">
+									<input type="text" id="enderecoformatted" name="enderecoformatted" value="a">
+									<input type="text" id="latitude" name="latitude" value="" hidden="true">
+									<input type="text" id="longitude" name="longitude" value="" hidden="true">
 									<button class="btn waves-effect waves-light red" type="submit" name="action">Cadastrar endereço</button>
 								</div>
 							</form>
@@ -56,11 +57,12 @@
 				<h4 class="white-text center headerborder">Meus Endereços</h4>
 				<div class="card headerborder">
 					<div class="col s10 m5 l12 card-content grey lighten-4 headerborder">
-						<div class="col s10 m5 l12 headerborder">
-							<table class="responsive-table"><thead><tr><th>Rua</th><th>País</th><th>Estado</th><th>CEP</th><th>Endereço Completo</th></tr></thead>
-							<?php listar($logado);?>
+						<div class="col s10 m5 l12 headerborder">	
+							<table class="responsive-table"><thead><tr><th>ID</th><th>Rua</th><th>País</th><th>Estado</th><th>CEP</th><th>Endereço Completo</th></tr></thead>
+								<?php listar($logado);?>
 							</table><br>
 							<a href="#modalendereco" class="waves-light btn modal-trigger" >Adicionar Endereço</a>
+							<a href="#modalexclusaoendereco" class="waves-light btn modal-trigger" >Excluir Endereço</a>
 						</div>
 					</div>
 				</div>
@@ -73,10 +75,24 @@
 				<div class="modal-footer">
 					<a href="#!" class="modal-close btn">Não</a>
 					<a href="/services/usuario/excluir.php"
-					class="waves-effect waves-green red btn-flat center-align" id="md1_YesBtn">Sim</a>
+					class="waves-light btn" id="md1_YesBtn">Sim</a>
 				</div>
 			</div>
-
+			<div id="modalexclusaoendereco" class="modal">
+				<div class="modal-content">
+					<center>
+						<div class="row">
+							<form action="/services/usuario/deletarEndereco.php" method="POST">
+								<div class="row">
+									<h6>Por favor, informe o ID do endereço a ser excluído: </h6><br>
+									<input type="number" id="idexclusao" name="idexclusao">
+									<button class="btn waves-effect waves-light red" type="submit" name="action">Excluir endereço</button>
+								</div>
+							</form>
+						</div>
+					</center>
+				</div>
+			</div>
 		</div>
 	</div>
 	<?php 
@@ -88,13 +104,27 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="js/materialize.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
+	
 	<script>
 		$(document).ready(function(){
 			$('.modal').modal();
 		});
 
 	</script>
-	<?php require("services/usuario/cadastrarEnderecos.php"); ?>
+<!--	<script type="text/javascript">
+		function listarEnderecosAPI() {
+			var lat = document.getElementById("latitude").value;
+			var lon = document.getElementById("longitude").value;
+			var geocoder  = new google.maps.Geocoder();             // create a geocoder object
+			var location  = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);    // turn coordinates into an object          
+			geocoder.geocode({'latLng': location}, function (results, status) {
+			if(status == google.maps.GeocoderStatus.OK) {           // if geocode success
+			var add=results[0].formatted_address;  
+			}       // if address found, pass to processing function
+			document.getElementById("enderecoformatted").value = document.write(add);
+		}
+	}
+</script> -->
 
 </body>
 </html>
